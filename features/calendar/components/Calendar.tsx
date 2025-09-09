d49b98ac48708c5f929ca9f8c6f84809458c3642
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-09-09 17:14:50 +0800

    feat: Implement selecting an event with placeholder handlers

--- features/calendar/components/Calendar.tsx
+++ features/calendar/components/Calendar.tsx
@@ -2,11 +2,33 @@
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import { DateSelectArg } from "@fullcalendar/core/index.js";
 
 const Calendar = () => {
+  const handleDateClick = (info: DateClickArg) => {
+    alert("Clicked" + info.dateStr);
+  };
+
+  const handleSelectEvents = (info: DateSelectArg) => {
+    alert(`Selected ${info.startStr} to ${info.endStr}`)
+  };
+
   return (
     <div className="rounded-2xl border-2 p-6 max-h-[600px] overflow-hidden basis-[70%]">
-      <FullCalendar height={"100%"} plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        height={"100%"}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        selectable
+        dateClick={handleDateClick}
+        select={handleSelectEvents}
+      />
     </div>
   );
 };
