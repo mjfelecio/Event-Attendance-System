339f6cc6e66d088e231837c8f270417186b3f500
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-09-12 21:27:26 +0800

    feat: Create reusable FormInput and ComboBox components

---
+++ globals/components/shared/ComboBox.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/globals/libs/shad-cn";
+import { Button } from "@/globals/components/shad-cn/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/globals/components/shad-cn/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/globals/components/shad-cn/popover";
+
+type ComboBoxValue = {
+  value: string;
+  label: string;
+};
+
+type ComboBoxProps = {
+  choices: ComboBoxValue[];
+  placeholder: string;
+  searchFallbackMsg: string;
+  onSelect: (value: ComboBoxValue) => void;
+};
+
+const ComboBox = ({
+  choices,
+  onSelect,
+  placeholder,
+  searchFallbackMsg,
+}: ComboBoxProps) => {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? choices.find((choice) => choice.value === value)?.label
+            : placeholder}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder={placeholder} className="h-9" />
+          <CommandList>
+            <CommandEmpty>{searchFallbackMsg}</CommandEmpty>
+            <CommandGroup>
+              {choices.map((choice) => (
+                <CommandItem
+                  key={choice.value}
+                  value={choice.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                    onSelect(choice)
+                  }}
+                >
+                  {choice.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === choice.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default ComboBox;
