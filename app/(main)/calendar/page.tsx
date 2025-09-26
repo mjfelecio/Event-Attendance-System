5abbd7a515dc4f8aac1d825d81ca95b750a38c1c
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-09-26 16:06:56 +0800

    refactor(calendar-page): Wrap functions in useCallback

--- app/(main)/calendar/page.tsx
+++ app/(main)/calendar/page.tsx
@@ -4,14 +4,14 @@ import Calendar from "@/features/calendar/components/Calendar";
 import EventDrawer from "@/features/calendar/components/EventDrawer";
 import EventsContainer from "@/features/calendar/components/EventsContainer";
 import { Event } from "@/globals/types/events";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 const CalendarPage = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [drawerMode, setDrawerMode] = useState<"create" | "edit">("create");
   const formData = useRef<Partial<Event>>(null);
 
-  const handleDrawerOpen = (event: Partial<Event> | null) => {
+  const handleDrawerOpen = useCallback((event: Partial<Event> | null) => {
     if (event?.id) {
       setDrawerMode("edit");
       formData.current = event;
@@ -20,19 +20,27 @@ const CalendarPage = () => {
       formData.current = event;
     }
     setIsDrawerOpen(true);
-  };
+  }, []);
 
-  const handleDrawerClose = () => {
+  const handleDrawerClose = useCallback(() => {
     formData.current = null;
     setIsDrawerOpen(false);
-  };
+  }, []);
+
+  const handleSelectDate = useCallback((start: Date, end: Date) => {
+    handleDrawerOpen({ start, end });
+  }, [handleDrawerOpen]);
+
+  const handleEditEvent = useCallback((event: Event) => {
+    handleDrawerOpen(event);
+  }, [handleDrawerOpen]);
 
   return (
     <div className="flex flex-1 bg-white p-8 gap-4 max-h-[680px]">
       <Calendar
         isDrawerOpen={isDrawerOpen}
-        onSelectDate={(start, end) => handleDrawerOpen({ start, end })}
-        onEditEvent={(event) => handleDrawerOpen(event)}
+        onSelectDate={handleSelectDate}
+        onEditEvent={handleEditEvent}
       />
       <EventsContainer onDrawerOpen={handleDrawerOpen} />
       <EventDrawer
