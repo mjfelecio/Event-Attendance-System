ead7d347d5070a0193b9bb2c0a439b7370081ada
Sagee <johnashgames23@gmail.com>
2025-10-04 00:52:51 +0800

    feat(categories): add constants for departments, strands, and houses
    Introduce COLLEGE_DEPARTMENTS, SHS_STRANDS, and HOUSES constants for use in
    student categorization.

---
+++ features/manage-list/constants/categories.ts
@@ -0,0 +1,37 @@
+export const COLLEGE_DEPARTMENTS = [
+  {
+    title: "Computer Studies",
+    abbreviation: "CS Dept.",
+  },
+  {
+    title: "Hotel Management",
+    abbreviation: "HM Dept.",
+  },
+  {
+    title: "Business Administration",
+    abbreviation: "BA Dept.",
+  },
+];
+
+export const SHS_STRANDS = {
+  academics: [
+    "STEM",
+    "HUMSS",
+    "ABM",
+    "GAS",
+  ],
+  tvl: [
+    "Programming",
+    "Animation",
+    "Home Economics",
+    "CSS",
+  ],
+};
+
+export const HOUSES = [
+  "GiallIO",
+  "Roxxo",
+  "Azul",
+  "Cahel",
+  "Vierrdy",
+];
