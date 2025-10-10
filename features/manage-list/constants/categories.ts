8c4760cc97ea9d70fcf3ec314826a8bbc3d0a5e4
Sagee <johnashgames23@gmail.com>
2025-10-11 00:16:27 +0800

    feat: add logos to categories and stats
    - Added logo property to categories
    - Added logo property to stats

--- features/manage-list/constants/categories.ts
+++ features/manage-list/constants/categories.ts
@@ -3,16 +3,19 @@ export const COLLEGE_DEPARTMENTS = [
     title: "Computer Studies",
     abbreviation: "CS Dept.",
     slug: "computer-studies",
+    logo: "/logos/department/CS.png",
   },
   {
     title: "Hotel Management",
     abbreviation: "HM Dept.",
     slug: "hotel-management",
+    logo: "/logos/department/HM.png",
   },
   {
     title: "Business Administration",
     abbreviation: "BA Dept.",
     slug: "business-administration",
+    logo: "/logos/department/AJBE.png",
   },
 ];
 
@@ -59,21 +62,26 @@ export const HOUSES = [
   {
     name: "Giallio",
     slug: "giallio",
+    logo: "/logos/house/GIALLIO.png",
   },
   {
     name: "Roxxo",
     slug: "roxxo",
+    logo: "/logos/house/ROXXO.png",
   },
   {
     name: "Azul",
     slug: "azul",
+    logo: "/logos/house/AZUL.png",
   },
   {
     name: "Cahel",
     slug: "cahel",
+    logo: "/logos/house/CAHEL.png",
   },
   {
     name: "Vierrdy",
     slug: "vierrdy",
+    logo: "/logos/house/VIERRDY.png",
   },
 ];
