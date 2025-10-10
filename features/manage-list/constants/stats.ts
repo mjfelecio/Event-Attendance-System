746a261e8b3bcd00bc51dbc0dba748c9daf2484c
Sagee <johnashgames23@gmail.com>
2025-10-11 00:16:27 +0800

    feat: add logos to categories and stats
    - Added logo property to categories
    - Added logo property to stats

--- features/manage-list/constants/stats.ts
+++ features/manage-list/constants/stats.ts
@@ -1,31 +1,31 @@
-import { Flame, UserRound, UsersRound } from "lucide-react";
+import { Flame } from "lucide-react";
 import { StudentStat } from "@/features/manage-list/types";
 
 export const STUDENT_STATS: StudentStat[] = [
   {
     title: "College Students",
-    icon: UserRound,
+    logo: "/logos/school/Logo_College_Department.png",
     align: "left",
     category: "college",
     value: 0,
   },
   {
     title: "SHS Students",
-    icon: UsersRound,
+    logo: "/logos/school/aclc.png",
     align: "right",
     category: "shs",
     value: 0,
   },
   {
     title: "All Students",
-    icon: Flame,
+    logo: "/logos/school/logo.png",
     align: "left",
     category: "all",
     value: 0,
   },
   {
     title: "House Members",
-    icon: Flame,
+    logo: "/logos/school/cup.png",
     align: "right",
     category: "house",
     value: 0,
