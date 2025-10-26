2c866f12678deca809a475fb85128b9099d77622
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-10-26 17:46:55 +0800

    feat: Add sonner/toast component to the project

--- app/(main)/attendance/page.tsx
+++ app/(main)/attendance/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import AttendancePageHeader from "@/features/attendance/components/AttendancePageHeader";
 import ScannerSection from "@/features/attendance/components/ScannerSection";
 import { Event } from "@/globals/types/events";
 import AttendanceRecordsTable from "@/features/attendance/components/AttendanceRecordsTable";
-import { fetchEventRecords } from "@/globals/hooks/useRecords";
 
 const AttendancePage = () => {
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
