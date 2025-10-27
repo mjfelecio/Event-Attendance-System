2f87a2b7fd494eb856fc8b5d12eb2df7a4aa9745
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-10-27 18:53:56 +0800

    feat: Replace react-qr-barcode-scanner libary with react-qr-scanner
    The old library was buggy and slow

--- app/api/records/route.ts
+++ app/api/records/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/globals/libs/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export async function POST(req: Request) {
   try {
@@ -11,11 +12,25 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ record }, { status: 201 });
   } catch (error) {
-    console.error("Failed to create record", error);
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      return NextResponse.json(
+        {
+          message: "Record already exists.",
+          error: {
+            code: "UNIQUE_CONSTRAINT",
+          },
+        },
+        { status: 409 }
+      );
+    }
+
+    // console.error("Failed to create record", error);
     return NextResponse.json(
       { message: "Failed to create record." },
       { status: 500 }
     );
   }
 }
-
