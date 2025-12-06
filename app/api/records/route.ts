89730bc7beff8faac097daef9d96296a2135b02f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-12-06 19:46:09 +0800

    feat: Implement flexible GET route for records
    This allows you to fetch a record(s):
         - from a specific event and student
         - all records of an event
         - all records of a student
         - all records
    
    It accomplishes this using search params. Centralizing this reduces the api file bloat and code duplication.
    And will result in easier maintanability

--- app/api/records/route.ts
+++ app/api/records/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/globals/libs/prisma";
 
 export async function POST(req: Request) {
@@ -26,6 +26,60 @@ export async function POST(req: Request) {
     console.error("Failed to create record: ", error);
     return NextResponse.json(
       { message: "Failed to create record." },
+      { status: 500 }
+    );
+  }
+}
+
+/**
+ * Fetches a record based on an eventId and a studentId
+ */
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const eventId = searchParams.get("eventId");
+  const studentId = searchParams.get("studentId");
+
+  try {
+    // Fetch a student record from a specific event
+    if (eventId && studentId) {
+      const record = await prisma.record.findUnique({
+        where: { eventId_studentId: { eventId, studentId } },
+      });
+
+      return NextResponse.json(record, { status: 200 });
+    }
+
+    // Fetch all records of an event
+    if (eventId) {
+      const records = await prisma.record.findMany({
+        where: { eventId: eventId },
+      });
+
+      return NextResponse.json(records, { status: 200 });
+    }
+
+    // Fetch all records of a student, from all events
+    if (studentId) {
+      const records = await prisma.record.findMany({
+        where: { studentId: studentId },
+      });
+
+      return NextResponse.json(records, { status: 200 });
+    }
+
+    // When no searchParams is found, fetch all records
+    const allRecords = await prisma.record.findMany();
+
+    return NextResponse.json(allRecords, { status: 200 });
+  } catch (error: any) {
+    console.error("Error fetching record:", error);
+
+    return NextResponse.json(
+      {
+        error:
+          error.message ??
+          "Unknown server error occured while fetching records",
+      },
       { status: 500 }
     );
   }
