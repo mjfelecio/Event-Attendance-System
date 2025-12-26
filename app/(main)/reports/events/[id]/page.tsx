6897d39d7b24f90e156b260fe3ab7847c7d20860
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-12-26 14:57:29 +0800

    feat: Implement exporting attendance records of event to csv
    - Add react-papaparser to parse JSON to CSV and vice-versa
    - Implement useDataExport to handle reusable logic for fetching data from a specific link and a button to start the export process
    - Create ExportButton, a dumb button that serves as the interface to export the data

--- app/(main)/reports/events/[id]/page.tsx
+++ app/(main)/reports/events/[id]/page.tsx
@@ -10,89 +10,99 @@ import { readableDate } from "@/globals/utils/formatting";
 import { capitalize } from "lodash";
 import { useParams } from "next/navigation";
 import AttendanceRecordsTable from "@/features/attendance/components/AttendanceRecordsTable";
+import ExportButton from "@/globals/components/shared/buttons/ExportButton";
+import { useDataExport } from "@/globals/hooks/useDataExport";
 
 const EventReportsPage = () => {
-	const { id } = useParams();
-	const eventId = String(id);
-	const { data: event, isLoading: isEventLoading } = useFetchEvent(eventId);
-	const { data: eventStats, isLoading: isStatsLoading } = useStatsOfEvent(
-		eventId
-	);
+  const { id } = useParams();
+  const eventId = String(id);
+  const { data: event, isLoading: isEventLoading } = useFetchEvent(eventId);
+  const { data: eventStats, isLoading: isStatsLoading } =
+    useStatsOfEvent(eventId);
 
-	// Compute attendance rate
-	const attendanceRate = useMemo(() => {
-		if (!eventStats || !eventStats.eligible || eventStats.eligible === 0)
-			return undefined;
-		return Number(
-			((eventStats.present / eventStats.eligible) * 100).toPrecision(4)
-		);
-	}, [eventStats]);
+  const { isExporting, exportData } = useDataExport({
+    apiUrl: `/api/events/${eventId}/records`,
+    filename: "attendance_records",
+  });
 
-	if (!event) {
-		return <p className="text-5xl">Loading</p>;
-	}
+  // Compute attendance rate
+  const attendanceRate = useMemo(() => {
+    if (!eventStats || !eventStats.eligible || eventStats.eligible === 0)
+      return undefined;
+    return Number(
+      ((eventStats.present / eventStats.eligible) * 100).toPrecision(4)
+    );
+  }, [eventStats]);
 
-	return (
-		<div className="flex flex-col gap-4 p-6 items-center border-2 border-gray-300 rounded-md flex-2 overflow-hidden">
-			<h3 className="text-4xl font-semibold">{event.title}</h3>
+  if (!event) {
+    return <p className="text-5xl">Loading</p>;
+  }
 
-			{/* Stats Cards */}
-			<div className="flex flex-wrap h-fit gap-6">
-				<DataCard
-					title="Present"
-					subtitle="Students checked in"
-					icon={IoMdCheckmarkCircleOutline}
-					value={eventStats?.present}
-					isLoading={isStatsLoading}
-				/>
-				<DataCard
-					title="Total Registered"
-					subtitle="Eligible attendees"
-					icon={FaUserGroup}
-					value={eventStats?.eligible}
-					isLoading={isStatsLoading}
-				/>
-				<DataCard
-					title="Attendance Rate"
-					subtitle="Current percentage"
-					icon={VscPercentage}
-					value={attendanceRate}
-					isLoading={isStatsLoading}
-					isPercentage
-				/>
-			</div>
+  return (
+    <div className="flex flex-col gap-4 p-6 items-center border-2 border-gray-300 rounded-md flex-2 overflow-hidden">
+      {/* Header */}
+      <div className="flex w-full items-center justify-center">
+        <h3 className="text-4xl font-semibold">{event.title}</h3>
+        <ExportButton
+          onExport={exportData}
+          isLoading={isExporting}
+          label="Export to CSV"
+        />
+      </div>
 
-			{/* Event Details */}
-			<div className="border w-full rounded-md p-4">
-				<p className="text-lg">
-					Organized by:{" "}
-					<span className="font-semibold">{event.userId}</span>
-				</p>
-				<p className="text-lg">
-					Conducted at:{" "}
-					<span className="font-semibold">
-						{readableDate(event.start)}
-					</span>
-				</p>
-				<p className="text-lg">
-					Participant groups:{" "}
-					<span className="font-semibold">{event.includedGroups}</span>
-				</p>
-				<p className="text-lg">
-					Location:{" "}
-					<span className="font-semibold">{event.location}</span>
-				</p>
-				<p className="text-lg">
-					Type:{" "}
-					<span className="font-semibold">
-						{capitalize(event.category)} Event
-					</span>
-				</p>
-			</div>
+      {/* Stats Cards */}
+      <div className="flex flex-wrap h-fit gap-6">
+        <DataCard
+          title="Present"
+          subtitle="Students checked in"
+          icon={IoMdCheckmarkCircleOutline}
+          value={eventStats?.present}
+          isLoading={isStatsLoading}
+        />
+        <DataCard
+          title="Total Registered"
+          subtitle="Eligible attendees"
+          icon={FaUserGroup}
+          value={eventStats?.eligible}
+          isLoading={isStatsLoading}
+        />
+        <DataCard
+          title="Attendance Rate"
+          subtitle="Current percentage"
+          icon={VscPercentage}
+          value={attendanceRate}
+          isLoading={isStatsLoading}
+          isPercentage
+        />
+      </div>
 
-			<AttendanceRecordsTable selectedEvent={event} />
-		</div>
-	);
+      {/* Event Details */}
+      <div className="border w-full rounded-md p-4">
+        <p className="text-lg">
+          Organized by: <span className="font-semibold">{event.userId}</span>
+        </p>
+        <p className="text-lg">
+          Conducted at:{" "}
+          <span className="font-semibold">{readableDate(event.start)}</span>
+        </p>
+        <p className="text-lg">
+          Participant groups:{" "}
+          <span className="font-semibold">{event.includedGroups}</span>
+        </p>
+        <p className="text-lg">
+          Location: <span className="font-semibold">{event.location}</span>
+        </p>
+        <p className="text-lg">
+          Type:{" "}
+          <span className="font-semibold">
+            {capitalize(event.category)} Event
+          </span>
+        </p>
+      </div>
+
+      <AttendanceRecordsTable selectedEvent={event} />
+    </div>
+  );
 };
 
 export default EventReportsPage;
